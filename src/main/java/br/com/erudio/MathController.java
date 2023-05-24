e6432df1662dcc7c6a63b928fcd752f35a85862e
luishenrique12 <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-05-23 22:50:00 -0300

    Revert "Merge pull request #1 from LuisHenriqueSantos/Origen/55.0603-Definindo-a-lógica-da-nossa-aplicação"
    This reverts commit 408bc1d620dca398e567d0151166852ff9932acf, reversing
    changes made to 5a142a4462ffb7bfece30a92baf14e05b70d3a5b.

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -16,22 +16,8 @@ public class MathController {
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
-    ) throws Exception {
-
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
-        }
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
-    }
-
-    private Double convertToDouble(String strNumber){
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    private boolean isNumeric(String strNumber) {
-        // TODO Auto-generated method stub
-        return false;
+    ) {
+        return 1D;
     }
 }
 
